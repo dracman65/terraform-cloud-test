d83bf93337f138d76173aaa9ce3804920659532b
dracman65 <dracman65@gmail.com>
2025-05-30 17:40:08 -0400

    Versions FMT

--- TF-CLI-Linux/versions.tf
+++ TF-CLI-Linux/versions.tf
@@ -2,18 +2,18 @@
 
 ##################################
 ### Providers - Cloud Backend ###
-terraform { 
-  cloud {    
+terraform {
+  cloud {
     organization = "DSD-Terraform-Test"
-    workspaces { 
-      name = "Tf-Cloud-CLI" 
-    } 
-  } 
+    workspaces {
+      name = "Tf-Cloud-CLI"
+    }
+  }
 }
 provider "azurerm" {
   # subscription_id = "6e83f8e1-bcf9-4cf5-8eb2-c9585391d4ee"
   # tenant_id       = "fa3f5c87-6b26-4240-9f12-c4679a19fed9"
   oidc_request_token = true
-  use_oidc = true
+  use_oidc           = true
   features {}
 }
