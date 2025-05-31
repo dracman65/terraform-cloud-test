42b53afd6617acda463450b7c477df1d9c0c26f2
dracman65 <dracman65@gmail.com>
2025-05-31 11:43:28 -0400

    Provider AWS update

--- AWS-Storage/provider.tf
+++ AWS-Storage/provider.tf
@@ -1,27 +1,38 @@
 
-terraform {
+# terraform {
 
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "> 3.5.1"
-    }
-  }
-}
-
-# provider "aws" {
-#   access_key = var.access_key
-#   secret_key = var.secret_key
-#   region     = var.region
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 5.0"
+#     }
+#     random = {
+#       source  = "hashicorp/random"
+#       version = "> 3.5.1"
+#     }
+#   }
 # }
+
+# # provider "aws" {
+# #   access_key = var.access_key
+# #   secret_key = var.secret_key
+# #   region     = var.region
+# # }
 
 provider "aws" {
   region                   = var.aws_region
-  shared_credentials_files = ["C:/Users/Digital_Dave/.aws/credentials"]
+#   shared_credentials_files = ["C:/Users/Digital_Dave/.aws/credentials"]
   profile                  = "default"
   allowed_account_ids      = [var.aws_account_id]
+}
+
+terraform { 
+  cloud { 
+    
+    organization = "DSD-Terraform-Test" 
+
+    workspaces { 
+      name = "AWS-CLI-Workflow" 
+    } 
+  } 
 }
